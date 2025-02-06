fd940be37de3d5fa9472338097d2209791a31041
simon-shao-ics4u1-03-25 <simon.shao@student.tdsb.on.ca>
2025-02-06 19:54:03 +0000

    Finish Assignment

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -14,37 +14,70 @@ public class PracticeProblem {
 
 	public static void q1() {
 		//Write question 1 code here
-		
+		Scanner input = new Scanner(System.in);
+		System.out.print("In: ");
+		String word = input.nextLine();
+		System.out.println(word.charAt(0));
+
 	}
 
 	public static void q2() {
 		//Write question 2 code here
-		
+		Scanner input = new Scanner(System.in);
+		System.out.print("In: ");
+		boolean trueFalse = input.nextBoolean();
+		input.nextLine();
+		System.out.println(!trueFalse);
 	}
 
 	public static void q3() {
 		//Write question 3 code here
-		
+		Scanner input = new Scanner(System.in);
+		System.out.print("In: ");
+		int num = input.nextInt();
+		input.nextLine();
+		System.out.println(num > 5);
 	}
 
 	public static void q4() {
 		//Write question 4 code here
-		
+		Scanner input = new Scanner(System.in);
+		System.out.print("In: ");
+		double number = input.nextDouble();
+		input.nextLine();
+		System.out.println(number <= 2 && number >= -2);
 	}
 
 	public static void q5() {
 		//Write question 5 code here
-		
+		Scanner input = new Scanner(System.in);
+		System.out.print("In: ");
+		String word = input.nextLine();
+		System.out.println(word.equals("Hello World"));
 	}
 
 	public static void q6() {
 		//Write question 6 code here
-		
+		Scanner input = new Scanner(System.in);
+		System.out.print("In: ");
+		int num = input.nextInt();
+		input.nextLine();
+		System.out.print("In: ");
+		int num1 = input.nextInt();
+		input.nextLine();
+		System.out.println(num <= num1);
 	}
 
 	public static void q7() {
 		//Write question 7 code here
-		
+		Scanner input = new Scanner(System.in);
+		System.out.print("In: ");
+		double number = input.nextDouble();
+		input.nextLine();
+		System.out.print("In: ");
+		double number1 = input.nextDouble();
+		input.nextLine();
+		System.out.println(number > number1);
 	}
 
 }
